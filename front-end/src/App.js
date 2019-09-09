452bdd02260ad18a20e0c8254d17263e597e75bc
github username <github email address>
2019-09-09 16:22:53 -0400

    Created Navbar component.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Redirect, Switch, withRouter } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      
+
     </div>
   );
 }
---
+++ front-end/src/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navigation(props){
+  return(
+    <div>
+    Home
+    Stories
+    Characters
+    </div>
+  )
+}
+
+export default Navigation;
