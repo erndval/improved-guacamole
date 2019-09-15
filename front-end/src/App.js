3e3e98aaecab2569beceebaa3ff736d38482ff53
github username <github email address>
2019-09-15 19:21:57 -0400

    Added Title component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
+
 import Navigation from './Navigation'
+import Title from './Title'
 
 function App() {
   return (
     <div className="App">
+      <Route path="/" component={Title} />
       <Route path="/" component={Navigation} />
     </div>
   );
