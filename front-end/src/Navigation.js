9da13024c09516097a81176d1185fb10b657f33d
github username <github email address>
2019-10-29 23:37:48 -0400

    Tinkering with CSS for a off canvas nav var.

--- front-end/src/Navigation.js
+++ front-end/src/Navigation.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 function Navigation(props){
   return(
-    <div role="list" class="ui list">
+    <div class="ui left vertical menu sidebar">
       <div role="listitem" class="item">Home</div>
-      <Link to="/content" role="listitem" class="item"><div>Stories</div></Link>
+      <Link to="/content" class="item"><div>Stories</div></Link>
       <Link to="/blog" role="listitem" class="item"><div>Blog</div></Link>
     </div>
 
